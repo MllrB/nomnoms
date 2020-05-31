b10c7f56b9539df1b0f4a4339f5f24e664e99f48
Brian Miller <brian@theinkshop.ie>
2020-05-31 11:15:49 +0100

    Ingredients and steps data now being  processed into list of dictionaries and a list respectively before being sent to mongodb

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import ast
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -31,11 +32,31 @@ def create_recipe():
 
     return render_template('create_recipe.html', measurements=measurements_list, categories=categories, recipe_info=extra_info)
 
-@app.route('/add_recipe', methods=['POST'])
+@app.route('/add_recipe/', methods=['POST'])
 def add_recipe():
+    # Adding a new recipe document to database
     new_recipe = mongo.db.scrambledeggs
     if request.method == 'POST':
-        new_recipe.insert_one(request.form.to_dict())
+        recipe_holder = request.form.to_dict()
+        # processing steps info from <textarea> into an array so each step 
+        # can be treated separately upon retrieval from mongodb
+        steps_holder = recipe_holder['steps']
+        steps = steps_holder.split('<br>')
+        if len(steps) >= 1 :
+            steps.pop()
+        recipe_holder['steps'] = steps
+        # processing ingredients/quantity/measurement info from <textarea> 
+        # into an array so each step can be treated separately upon retrieval from mongodb
+        ingredients_holder = recipe_holder['ingredients']
+        ingredients = ingredients_holder.split('<br>')
+        if len(ingredients) >= 1:
+            ingredients.pop()
+            for i, val in enumerate(ingredients):
+                ingredients[i] = ast.literal_eval(val)            
+        recipe_holder['ingredients'] = ingredients
+
+        new_recipe.insert_one(recipe_holder)
+
     return redirect(url_for('create_recipe'))
 
 
